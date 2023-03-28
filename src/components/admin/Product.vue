fc1734617da7ee8c5b5e905422cebe595f2451cb
alirezamou <am808036@gmail.com>
2023-03-28 15:31:24 +0330

    add markup for Product comp and fix the  to

--- src/components/admin/Product.vue
+++ src/components/admin/Product.vue
@@ -1,7 +1,107 @@
 <template>
-    <div>
-        Admin Product
-    </div>
+    <section class="section container">
+        <h1 class="title is-2">Admin</h1>
+        <h2 class="subtitle is-4">Products</h2>
+
+        <a @click="addingProduct = true" class="button is-primary">
+            <fa-icon icon="fa-solid fa-plus"></fa-icon> Add Product
+        </a>
+
+        <div class="box" v-if="addingProduct">
+            <form @submit.prevent="addProduct()">
+                <div class="field">
+                    <button type="submit" class="button is-success">Save</button>
+                </div>
+                <div class="field">
+                    <label for="" class="label">Name</label>
+                    <div class="control">
+                        <input type="text" class="input" v-model="newProduct.name">
+                    </div>
+                </div>
+                <div class="field">
+                    <label for="" class="label">Description</label>
+                    <div class="control">
+                        <input type="text" class="input" v-model="newProduct.description">
+                    </div>
+                </div>
+                <div class="field">
+                    <label for="" class="label">Image</label>
+                    <div class="control">
+                        <input type="text" class="input" v-model="newProduct.image">
+                    </div>
+                </div>
+            </form>            
+        </div>
+
+        <div class="box" v-for="(product, index) in products" :key="product.id">
+            <h2 class="title-is-5">{{ product.data.name }}
+                <span class="is-pulled-right">
+                    <a @click="editProduct(index)" class="button is-primary">
+                        <fa-icon icon="fa-solid fa-edit"></fa-icon>
+                    </a>
+                    <a @click="deleteProduct(index)" class="button is-danger">
+                        <fa-icon icon="fa-solid fa-times"></fa-icon>
+                    </a>
+                </span>
+            </h2>
+            <form @submit.prevent="saveProduct(index)" v-if="product.editing">
+                <div class="field">
+                    <button type="submit" class="button is-success">Save</button>
+                </div>
+                <div class="field">
+                    <label for="" class="label">Name</label>
+                    <div class="control">
+                        <input type="text" class="input" v-model="product.data.name">
+                    </div>
+                </div>
+                <div class="field">
+                    <label for="" class="label">Description</label>
+                    <div class="control">
+                        <input type="text" class="input" v-model="product.data.description">
+                    </div>
+                </div>
+                <div class="field">
+                    <label for="" class="label">Image</label>
+                    <div class="control">
+                        <input type="text" class="input" v-model="product.data.image">
+                    </div>
+                </div>
+            </form>
+            <form @submit.prevent="addSize(index)" v-if="product.editing">
+                <table class="table is-narrow">
+                    <caption class="title is-5">Sizes</caption>
+                    <thead>
+                        <tr>
+                            <th>Weight</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(size, sizeIndex) in product.data.sizes" :key="sizeIndex">
+                            <td><input type="text" class="input" v-model="size.weight"></td>
+                            <td><input type="text" class="input" v-model="size.name"></td>
+                            <td><input type="text" class="input" v-model="size.price"></td>
+                            <td><a @click="delete_size(index, sizeIndex)" class="button is-danger">
+                                <fa-icon icon="fa-solid fa-times"></fa-icon>
+                            </a></td>
+                        </tr>
+
+                        <tr>
+                            <td><input type="text" class="input" v-model="newSize.weight"></td>
+                            <td><input type="text" class="input" v-model="newSize.name"></td>
+                            <td><input type="text" class="input" v-model="newSize.price"></td>
+                            <td><button type="submit" class="button is-success">
+                                <fa-icon icon="fa-solid fa-plus"></fa-icon>
+                            </button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </form>
+        </div>
+
+    </section>
 </template>
 <script>
 import db from "@/library/Database";
@@ -28,7 +128,7 @@ export default {
     methods: {
         addProduct() {
 
-            this.db.add_product(this.newProduct)
+            db.add_product(this.newProduct)
             .then((product) => {
                 this.products.unshift({
                     id: product.id,
