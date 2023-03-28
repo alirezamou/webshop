6347e283e2cb426aacd0ce599548a906a0027fec
alirezamou <am808036@gmail.com>
2023-03-28 15:30:48 +0330

    add markup and codes for Login comp

--- src/components/admin/Login.vue
+++ src/components/admin/Login.vue
@@ -1,11 +1,58 @@
 <template>
-    <div>
-        Admin Login
+  <form @submit.prevent="login" class="container">
+    <div class="field">
+      <label class="label">Email</label>
+      <div class="control">
+        <input class="input" v-model="email" type="email">
+      </div>
     </div>
+    <div class="field">
+      <label class="label">Password</label>
+      <div class="control">
+        <input class="input" v-model="password" type="password">
+      </div>
+    </div>
+    <p v-if="error">{{ error }}</p>
+    <div class="control">
+      <button class="button is-primary" type="submit">Submit</button>
+    </div>
+  </form>
 </template>
+
 <script>
+import "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import { FirebaseApp } from "@/library/Database";
+
+let auth = getAuth(FirebaseApp);
+
 export default {
-    name: "AdminLogin"
+    name: "AdminLogin",
+    data() {
+        return {
+            email: "",
+            password: "",
+            error: null,
+        };
+    },
+    methods: {
+        login() {
+            console.log("in login");
+            signInWithEmailAndPassword(auth, this.email, this.password)
+            .then(() => console.log("signed in"))
+            .catch((error) => {
+                this.error = error.message;
+                console.log(error);
+            });
+
+            onAuthStateChanged(auth ,(user) => {
+                console.log("state changed: ", user);
+                if(user) {
+                    this.$router.push("/admin/products");
+                }
+            })
+        }
+    }
 }
 </script>
 <style>
