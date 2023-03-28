37efd0f69d2d0c0307eadf5c08295837d20f35eb
alirezamou <am808036@gmail.com>
2023-03-28 12:17:34 +0330

    add faTimes and faEdit icon

--- src/main.js
+++ src/main.js
@@ -9,9 +9,9 @@ const app = createApp(App);
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faPlus, faEdit, faTimes } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faPlus);
+library.add(faPlus, faEdit, faTimes);
 
 app.component("fa-icon", FontAwesomeIcon);
 
