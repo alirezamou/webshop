e30ba185439f6a7e56a15168cd389aa318c844d7
alirezamou <am808036@gmail.com>
2023-03-28 20:18:56 +0330

    add Products comp and add header

---
+++ src/components/Products.vue
@@ -0,0 +1,32 @@
+<template>
+    <section class="hero is-success is-medium">
+        <div class="hero-body">
+            <div class="container">
+                <h1 class="title is-1">Ye Artisenal Bread Shoppe</h1>
+                <h2 class="subtitle is-1">Hommade breads for ridiculous prices</h2>
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.hero {
+    background: linear-gradient(
+        rgba(0, 0, 0, 0.35),
+        rgba(0, 0, 0, 0.35)
+    ), url("../assets/background.jpg"),
+    center;
+    font-family: 'Amatic SC', cursive;
+}
+
+.hero .title {
+    font-size: 7rem;
+    font-weight: normal;
+}
+
+</style>
