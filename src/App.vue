82ccab5a633bf3ac1a0bebf103a9b42a7dbbb019
alirezamou <am808036@gmail.com>
2023-03-29 13:09:32 +0330

    add a navbar in App comp so we can access to Cart comp

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,30 @@
 <script>
 export default {
-  name: "App"
+  name: "App",
+  computed: {
+    total() {
+      return this.$store.getters.totalItems;
+    }
+  }
 }
 </script>
 
 <template>
+  <nav class="navbar" role="navigation">
+    <div class="container">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item">Ye Artisenal Bread Shoppe</router-link>
+      </div>
+
+      <div class="navbar-menu">
+        <div class="navbar-end">
+          <router-link to="/cart" class="navbar-item">
+            <fa-icon icon="fa-solid fa-shopping-cart">Cart {{ total }}</fa-icon>
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </nav>
   <router-view />
 </template>
 
